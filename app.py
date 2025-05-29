80293408241e4eaa7804d7dc9590745f1802fd50
asifmostafa18 <asifmostafa915@gmail.com>
2025-05-29 10:53:56 +0600

    Second Push

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-    return a + b
+def add_numbers(a, b, c):
+    return a + b +c
 
 def subtract_numbers(a, b):
     return a - b
@@ -7,7 +7,8 @@ def subtract_numbers(a, b):
 def main():
     x = 5
     y = 3
-    print("Addition:", add_numbers(x, y))
+    z = 5
+    print("Addition:", add_numbers(x, y,z))
     print("Subtraction:", subtract_numbers(x, y))
 
 if __name__ == "__main__":
