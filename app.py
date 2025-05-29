a00e595771b14eccd8fad2073a9dc3641c70a109
asifmostafa18 <asifmostafa915@gmail.com>
2025-05-29 11:53:45 +0600

    Fifth Push

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b, c):
-    return a + b +c
+def add_numbers(a, b, c, d):
+    return a + b +c +d
 
 def subtract_numbers(a, b):
     return a - b
@@ -8,8 +8,10 @@ def main():
     x = 5
     y = 3
     z = 5
-    print("Addition:", add_numbers(x, y,z))
+    w = 10
+    print("Addition:", add_numbers(x, y,z,w))
     print("Subtraction:", subtract_numbers(x, y))
+    print("Just to Confirm")
 
 if __name__ == "__main__":
     main()
